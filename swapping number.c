0d59ffa2d972a1da257ff4b4b86fcf608e230cd7
sumeghbhamre2006-stack <sumeghbhamre2006@gmail.com>
2025-09-25 19:36:01 +0530

    Create swapping number.c

---
+++ swapping number.c
@@ -0,0 +1,13 @@
+//Develop A C program for swapping of two numbers.
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf(“Enter two numbers:”);
+scanf(“%d%d,&a&b);
+c=a;
+a=b;
+b=c;
+printf(“\n The two numbers after swapping are:%d,%d,a,b);
+return 0;
+}
